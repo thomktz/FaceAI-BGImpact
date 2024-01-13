1a23d3b84c4088daaeddcbd94b775beeb3f727b4
Thomas Kientz <thomas@kientz.net>
2024-01-13 20:00:22 +0100

    tweak config

--- faceai_bgimpact/configs/default_vae_config.py
+++ faceai_bgimpact/configs/default_vae_config.py
@@ -2,8 +2,8 @@ config = {
     "dataset_name": "ffhq_raw",
     "lr": 0.0001,
     "latent_dim": 256,
-    "batch_size": 128,
+    "batch_size": 64,
     "num_epochs": 400,
-    "save_interval": 5,
-    "image_interval": 50,
+    "save_interval": 2,
+    "image_interval": 100,
 }
