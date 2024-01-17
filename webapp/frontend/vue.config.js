e7991db1df08d1b9e4ae15c51358b3ba8ebf1c7d
Thomas Kientz <thomas@kientz.net>
2024-01-17 23:46:32 +0100

    Ports update 8080->8082

--- webapp/frontend/vue.config.js
+++ webapp/frontend/vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     host: "0.0.0.0",
-    port: 8080,
+    port: 8082,
     allowedHosts: "all",
     server: process.env.VUE_APP_SERVER,
   },
